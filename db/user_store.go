8628dea4554ac841c0cdce0401d28dff9dac3a79
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2026-08-19 18:50:01 +0330

    Fix get all users

--- db/user_store.go
+++ db/user_store.go
@@ -50,8 +50,8 @@ func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
 	}
 
 	var users []*types.User
-	if err := cur.Decode(&users); err != nil {
-		return []*types.User{}, nil
+	if err := cur.All(ctx, &users); err != nil {
+		return nil, err
 	}
 
 	return users, nil
